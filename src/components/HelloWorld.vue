3c889b591c0425926b9f83e255d86da15f858fef
Bobir Abdukhalilov <destbest94@gmail.com>
2019-04-28 16:59:31 +0300

    New project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-container app>
+        <v-layout>
+          <v-flex style="margin-right: 8px;">
+            <v-text-field label="Name" v-model="info.name"></v-text-field>
+          </v-flex>
+
+          <v-flex>
+            <v-text-field label="Phone" v-model="info.phone"></v-text-field>
+          </v-flex>
+          <v-btn @click="addPersonInfo">{{ btnText }}</v-btn>
+        </v-layout>
+      </v-container>
+
+      <v-container>
+        <v-list two-line v-if="persons.length">
+          <v-list-tile v-for="(person, i) in persons" :key="i">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ person.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ person.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-layout>
+                <v-btn @click="changePersonInfo(i)">📝</v-btn>
+                <v-btn @click="deletePersonInfo(i)">❌</v-btn>
+              </v-layout>
+            </v-list-tile-action>
+          </v-list-tile>
+          <v-divider></v-divider>
+        </v-list>
+      </v-container>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    change: false,
+    btnText: "Add",
+    persons: [],
+    info: {
+      name: "",
+      phone: ""
+    }
+  }),
+
+  methods: {
+    addPersonInfo: function() {
+      if (this.change) {
+        this.info = {
+          name: "",
+          phone: ""
+        };
+        this.change = false;
+        this.btnText = "Add";
+      } else {
+        if (this.info.name && this.info.phone) {
+          let obj = {
+            name: this.info.name,
+            phone: this.info.phone
+          };
+          this.persons.push(obj);
+        }
+
+        this.info.name = "";
+        this.info.phone = "";
+      }
+    },
+
+    deletePersonInfo: function(id) {
+      this.persons.splice(id, 1);
+    },
+
+    changePersonInfo: function(id) {
+      this.info = this.persons[id];
+      this.change = true;
+      this.btnText = "Change";
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
 </style>
